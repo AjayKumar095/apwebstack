6cfbf5ce0aff6e2308a925f1511c4f2de1159d94
Ajay <jaykumar002kori@gmail.com>
2025-11-13 21:09:23 +0530

    commit

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class MetaData(models.Model):
+    meta_title = models.TextField(max_length=60, editable=True, blank=False)
+    meta_description = models.TextField(max_length=156, editable=True, blank=False)
+    
+    
+
