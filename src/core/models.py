b275937f51fa1f79f651dec04cb0c9297da7532c
Ajay <jaykumar002kori@gmail.com>
2025-11-14 11:15:10 +0530

    commit

--- src/core/models.py
+++ src/core/models.py
@@ -6,5 +6,6 @@ class MetaData(models.Model):
     meta_title = models.TextField(max_length=60, editable=True, blank=False)
     meta_description = models.TextField(max_length=156, editable=True, blank=False)
     
-    
+    # class Meta:
+    #     abstract = True
 
