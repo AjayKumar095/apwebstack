81dbcf274abf6fa6630b1f1a758b218713987395
Ajay <jaykumar002kori@gmail.com>
2025-11-17 11:47:38 +0530

    commit

--- src/index/models.py
+++ src/index/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+## Home page hero section
+class HeroSection(models.Model):
+    
+    Heading = models.CharField(max_length=50, null=False, blank=False)
+    Description = models.CharField(max_length=260, null=False, blank=False)
+    Date_Created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        get_latest_by = 'Data_Created'
+    
+
