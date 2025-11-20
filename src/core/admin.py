df497d9d33e08ca644526c0da19e1f0c09c6a072
Ajay <jaykumar002kori@gmail.com>
2025-11-20 20:38:49 +0530

    commit

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Icon
+from .models import Icon, SectionType
 
 @admin.register(Icon)
 class IconAdmin(admin.ModelAdmin):
     search_fields = ("name", "class_name")
+    list_display = ("name", "class_name")
+    
+@admin.register(SectionType)
+class SectionTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "use_heading", "use_paragraph", "use_image", "use_bullets", "use_rows")
+    search_fields = ("name",)
