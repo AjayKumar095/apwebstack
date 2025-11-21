cd4980e96ff379363a45a93871116968f2131e69
Ajay <jaykumar002kori@gmail.com>
2025-11-21 20:27:54 +0530

    commit

--- src/index/views.py
+++ src/index/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from src.logger import log_error
 
-# Create your views here.
 
 def index(request):
-    return render(request=request, template_name="index/index.html")
+    
+    try:
+        return render(request=request, template_name="index/index.html")
+    except Exception as e:
+        context = {
+            "status": 404,
+            "error": "Page Not Found",
+            "message": "Sorry, the page you are looking for doesn’t exist or may have been moved.",
+            "page": "index"
+        }
+
+        log_error(
+            f"Error occurred while rendering index page.\n"
+            f"Exception: {type(e).__name__}\n"
+            f"Message: {e}"
+        )
+
+        return render(
+            request=request,
+            template_name="error/errors.html",
+            context=context,
+            status=404
+        )
