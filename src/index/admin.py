b63274e333a935e15d2069e2fd3880d14aef908a
Ajay <jaykumar002kori@gmail.com>
2025-11-22 12:49:46 +0530

    commit

--- src/index/admin.py
+++ src/index/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class WhyChooseUsRowInline(admin.TabularInline):
+    model = WhyChooseUsRow
+    extra = 1
+    fields = (
+        "icon", "icon_color",
+        "heading", "paragraph",
+        "image", "image_alt"
+    )
+
+@admin.register(HeroSection)
+class HeroSectionAdmin(admin.ModelAdmin):
+    list_display = ("Heading", "Description")
+    
+@admin.register(TechnologyLogo)
+class TechnologyLogoAdmin(admin.ModelAdmin):
+    list_display = ("img_alt")   
+    
+@admin.register(WhyChooseUs)
+class WhyChooseUsAdmin(admin.ModelAdmin):
+    list_display = ("service",)
+    inlines = [WhyChooseUsRowInline]
--- src/index/models.py
+++ src/index/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from core.models import BulletPointBase, RowBase
 
 # Create your models here.
 
@@ -7,10 +8,32 @@ class HeroSection(models.Model):
     
     Heading = models.CharField(max_length=50, null=False, blank=False)
     Description = models.CharField(max_length=260, null=False, blank=False)
-    Date_Created = models.DateTimeField(auto_now_add=True)
+    background_img = models.ImageField( upload_to="page/index/hero", null=False, blank=False)
+    img_alt = models.CharField( max_length=55, null=False, blank=False)
     
-    class Meta:
-        get_latest_by = 'Data_Created'
+    def __str__(self):
+        return f"{self.Heading}"
+
+class TechnologyLogo(models.Model):
+    background_img = models.ImageField( upload_to="page/index/techlogo", null=False, blank=False)
+    img_alt = models.CharField( max_length=55, null=False, blank=False)
+    
+    def __str__(self):
+        return f"{self.img_alt}" 
+    
+class WhyChooseUs(models.Model):
+    Heading = models.CharField(max_length=100, null=False, blank=False)
+    Description = models.CharField(max_length=455, null=False, blank=False)
+    
+    def __str__(self):
+        return f"{self.Heading}"
+    
+class WhyChooseUsRow(RowBase):
+    section = models.ForeignKey(WhyChooseUs, on_delete=models.CASCADE, related_name="rows")         
+    
+    
+           
+     
     
 
 
