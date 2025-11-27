9c798e58ba26de0a0bc29a15002c19ac23f9d7cc
Ajay <jaykumar002kori@gmail.com>
2025-11-27 16:11:17 +0530

    feat: add robots.txt for search engine optimization

--- src/src/urls.py
+++ src/src/urls.py
@@ -22,6 +22,7 @@ from django.contrib.sitemaps.views import sitemap
 
 from core.sitemaps import StaticViewSitemap
 from services.sitemaps import ServiceSitemap
+from django.views.generic import TemplateView
 
 sitemaps = {
     'static': StaticViewSitemap,
@@ -36,5 +37,10 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('policy/', include('core.urls')),
     path('sitemap.xml', sitemap, {"sitemaps": sitemaps}, name="django_sitemap"),
+        # robots.txt
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain")
+    ),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
