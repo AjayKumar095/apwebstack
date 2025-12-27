489e803c3830ea2fa6c73266f6b6098e86261396
Ajay <jaykumar002kori@gmail.com>
2025-12-27 17:09:59 +0530

    commit

--- src/src/settings.py
+++ src/src/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h(io5f$6!m04=k19urwl6xe9aeqlel&@a-8#4r2bp*8ee_7o^f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  # development
-# DEBUG = False  # production
+#DEBUG = True  # development
+DEBUG = False  # production
 
 SITE_ID = 1 # production 
 #SITE_ID = 3 # development 
